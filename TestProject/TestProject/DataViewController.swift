2ba622a717fa0dfdf5d57b72ec384cfcd048b60e
Surajit Nundy <nundys@gmail.com>
2017-05-13 23:43:47 +0530

    Adding default Xcode project

---
+++ TestProject/TestProject/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  TestProject
+//
+//  Created by Surajit Nundy on 13/05/17.
+//  Copyright © 2017 Raxa Health Information Services. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
